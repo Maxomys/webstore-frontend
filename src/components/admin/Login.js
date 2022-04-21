d852be57aab8b94cbb48024119cf7cdb6e63dd41
Zaphod <mistrz7losos@live.com>
2022-04-21 18:17:22 +0200

    updated view

--- src/components/admin/Login.js
+++ src/components/admin/Login.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import {  useState } from 'react';
 import '../styles/login.css'
 import UserService from 'services/UserService'
 import { useNavigate } from 'react-router-dom';
--- src/components/store/Navbar.js
+++ src/components/store/Navbar.js
@@ -1,5 +1,6 @@
 import { Disclosure } from '@headlessui/react';
-import { MenuIcon, ShoppingBagIcon, XIcon } from '@heroicons/react/outline'
+import { MenuIcon, ShoppingBagIcon, XIcon, UserCircleIcon } from '@heroicons/react/outline'
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const navigation = [
@@ -8,17 +9,31 @@ const navigation = [
   {name: 'Admin', href: '/admin'}
 ];
 
+
 function Navbar({ currentTab }) {
   
   const navigate = useNavigate();
+  
+  const [username, setUsername] = useState(undefined);
+
+  function checkUsername() {
+    const username = localStorage.getItem('username');
+    if (username) {
+      setUsername(username);
+    }
+  }
+
+  useEffect(() => {
+    checkUsername();
+  }, []);
 
   return ( 
 
     <Disclosure as='nav' className=''>
       {({open}) => (
         <>
-          <div className={'flex items-center justify-between h-14 max-w-screen-2xl mx-auto md:mt-3 bg-gradient-to-r from-cyan-500 to-emerald-500 p-4 rounded-xl shadow-md ' 
-            + (open && 'rounded-b-none')}>
+          <div className={'flex items-center justify-between h-14 max-w-screen-2xl mx-auto md:mt-3 bg-gradient-to-r from-cyan-500 to-emerald-500 p-4 rounded-xl shadow-md' 
+            + (open && ' rounded-b-none')}>
             <div className='flex items-center gap-3'>
               <ShoppingBagIcon className='h-7 w-7 text-white'/>
               <p className='text-white'>Store</p>
@@ -32,36 +47,48 @@ function Navbar({ currentTab }) {
                 ))}
               </div>
             </div>
-            <div className='hidden md:flex divide-x-2 divide-black divide-opacity-10'>
+            {username ? 
+              <div className='hidden md:flex md:flex-row md:items-center gap-2'>
+                <UserCircleIcon className='h-8 w-8 text-white'/>
+                <p className='font-normal text-white'>{username}</p>
+              </div>
+            : <div className='hidden md:flex divide-x-2 divide-black divide-opacity-10'>
                 <button className='bg-black bg-opacity-20 rounded-l-xl px-5 py-2 transition-opacity hover:bg-opacity-30' 
                   onClick={() => navigate('/login')}
                 >Login</button>
                 <button className='bg-black bg-opacity-20 rounded-r-xl px-5 py-2 transition-opacity hover:bg-opacity-30'>Register</button>
-            </div>
-            <div className='md:hidden self-center'>
+              </div>
+            }
+            <div className='md:hidden self-center flex items-center'>
               <Disclosure.Button>
                 {open ? (
-                  <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  <XIcon className="block h-6 w-6" aria-hidden="true"/>
                 ) : (
-                  <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  <MenuIcon className="block h-6 w-6" aria-hidden="true"/>
                 )}
               </Disclosure.Button>
             </div>
           </div>
           <Disclosure.Panel>
-            <div className='md:hidden flex flex-col items-start space-y-1 px-3 pt-2 pb-3 rounded-b-xl bg-gradient-to-r from-cyan-500 to-emerald-500 p-4'>
+            <div className='md:hidden flex flex-col items-start space-y-1 px-3 pt-2 pb-3 rounded-b-xl bg-gradient-to-r from-cyan-500 to-emerald-500 p-4 shadow-md'>
               {navigation.map(item => (
                 <button key={item.name} className={'hover:bg-black hover:bg-opacity-30 w-full rounded-xl px-4 py-2 transition-opacity text-left text-white '
-                  + (item.name === currentTab && 'bg-opacity-20 bg-black')}
+                  + (item.name === currentTab && ' bg-opacity-20 bg-black')}
                   onClick={() => navigate(item.href)}
                 >{item.name}</button>
               ))}
-              <div className='w-full border-t border-white'>
-                <button className='hover:bg-black hover:bg-opacity-30 w-full rounded-xl px-4 py-2 transition-opacity text-left text-white'
-                  onClick={() => navigate('/login')}
-                >Login</button>
-                <button className='hover:bg-black hover:bg-opacity-30 w-full rounded-xl px-4 py-2 transition-opacity text-left text-white'>Register</button>
-              </div>
+              {username ? 
+                <div className='flex flex-row items-center gap-2 ml-2 pt-3'>
+                  <UserCircleIcon className='h-8 w-8 text-white'/>
+                  <p className='font-normal text-white'>{username}</p>
+                </div>
+              : <div className='w-full border-t border-white'>
+                  <button className='hover:bg-black hover:bg-opacity-30 w-full rounded-xl px-4 py-2 transition-opacity text-left text-white'
+                    onClick={() => navigate('/login')}
+                  >Login</button>
+                  <button className='hover:bg-black hover:bg-opacity-30 w-full rounded-xl px-4 py-2 transition-opacity text-left text-white'>Register</button>
+                </div>
+              }
             </div>
           </Disclosure.Panel>
         </>
